fa1d13062ea9428588b726b8898f1186654ec973
kschneider726 <kschneider726@users.noreply.github.com>
2016-10-26 22:51:01 -0700

    upload0

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Test scripts.js
+console.log('hello!');
+
+// Test jQuery
+// $('img').hide();
+
+$('img').imageLens({
+    imageSrc: '/image-lens/img/watch-zoom.jpg',    
+    lensSize: 400,
+    borderColor: '#FFFF31'
+});
